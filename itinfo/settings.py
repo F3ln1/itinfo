c83f852dd48523796c3fe1c18bd497f4e10db9d6
F3ln1 <ikashkar@mail.ru>
2026-06-08 01:33:01 +0600

    Change STATIC_ROOT to env variable, point to /var/www/resonatenews/staticfiles

--- itinfo/settings.py
+++ itinfo/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.getenv('STATIC_ROOT', str(BASE_DIR / 'staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_TRUSTED_ORIGINS = [origin for origin in os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if origin]
