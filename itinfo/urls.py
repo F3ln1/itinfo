f0d90f60bc2fa2aaa572c5f119945cc1103f380e
F3ln1 <ikashkar@mail.ru>
2026-06-08 00:45:51 +0600

    Add 404 handler redirect to home

--- itinfo/urls.py
+++ itinfo/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = lambda request, exception: redirect('news:home')
