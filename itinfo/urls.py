1c1e7837a8cad54d07706cd2d4f29e28d4164ce7
F3ln1 <ikashkar@mail.ru>
2026-06-08 00:53:00 +0600

    Change admin URL to nimdaresonswen

--- itinfo/urls.py
+++ itinfo/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('nimdaresonswen/', admin.site.urls),
     path('', include('news.urls')),
 ]
 
